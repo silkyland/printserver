58c87176a0ec5917b19d9b0cbbd28d8956067fc6
Bundit Nuntates <silkyland@gmail.com>
2020-11-04 11:48:38 +0700

    update

--- print.js
+++ print.js
@@ -1,16 +1,29 @@
-const printer = require('printer');
-const fs = require('fs')
+var printer = require("printer"),
+    filename = process.argv[2] || __filename;
 
-var data = fs.readFileSync("output.pdf");
+console.log('platform:', process.platform);
+console.log('try to print file: ' + filename);
 
-printer.printDirect({
-    data: data,
-    printer: "ZDesigner GT800 (EPL)",
-    type: "PDF",
-    success: function (s) {
-        console.log(s)
+if( process.platform != 'win32') {
+  printer.printFile({filename:filename,
+    printer: process.env[3], // printer name, if missing then will print to default printer
+    success:function(jobID){
+      console.log("sent to printer with ID: "+jobID);
     },
-    error: function (err) {
-        console.log(err)
+    error:function(err){
+      console.log(err);
+    }
+  });
+} else {
+  // not yet implemented, use printDirect and text
+  var fs = require('fs');
+  printer.printDirect({data:fs.readFileSync(filename),
+    printer: process.env[3], // printer name, if missing then will print to default printer
+    success:function(jobID){
+      console.log("sent to printer with ID: "+jobID);
     },
-});
+    error:function(err){
+      console.log(err);
+    }
+  });
+}
